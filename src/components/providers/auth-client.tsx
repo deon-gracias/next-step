7ff27901942cfeecfe518bbc28be082e653c80db
deon-gracias <deongracias1@gmail.com>
2026-01-13 19:27:57 +0530

    Added extra trusted origins

--- src/components/providers/auth-client.tsx
+++ src/components/providers/auth-client.tsx
@@ -1,12 +1,8 @@
-import { createAuthClient } from "better-auth/react";
-import { organizationClient } from "better-auth/client/plugins";
-
-const baseURL =
-typeof window !== "undefined"
-? window.location.origin
-: process.env.APP_URL ?? "http://localhost:3000";
+import { createAuthClient } from "better-auth/react"; import { organizationClient } from "better-auth/client/plugins"; const baseURL = typeof window !== "undefined"
+  ? window.location.origin
+  : (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
 
 export const authClient = createAuthClient({
-plugins: [organizationClient()],
-baseURL: baseURL,
+  plugins: [organizationClient()],
+    : (process.env.VERCEL_URL ?? "http://localhost:3000");
 });
